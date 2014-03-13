18e7fd96402a86f25a19fb4b5e30cdb3a734e621
Martin <martin@livetheseasons.com>
2014-03-13 19:41:24 +0000

    start to formalise testing

--- tiddlers/test/test-graph-widget.js
+++ tiddlers/test/test-graph-widget.js
@@ -33,7 +33,7 @@ Tests the visjs graph widget wrapper (This does not use the visjs graph, but use
                     document: $tw.fakeDocument
           // variables: {currentTiddler: "TiddlerOne"}
         });
-        mockGraph = jasmine.createSpyObj('mockGraph', ['on', 'setItems', 'setGroups', 'setOptions', 'setCustomTime', 'setWindow']);
+        mockGraph = jasmine.createSpyObj('mockGraph', ['on', 'setData']);
         widgetNode.mockGraph = mockGraph;
       }
 
@@ -79,31 +79,107 @@ Tests the visjs graph widget wrapper (This does not use the visjs graph, but use
       // Test link loop (one -> two -> one, but not if inverse relationship)
       it("should report unexpected attributes", function() {
         // Construct the widget node
-        createAndRenderWidgetNode('<$visjsgraph list="TiddlerTwo GettingStartedzzzz" madeUpAttr="created"/>');
+        createAndRenderWidgetNode('<$visjsgraph madeUpAttr="fred"/>');
         // Test the rendering
         expect(wrapper.innerHTML).toBe("<p><div class='widget-error'>visjsgraph: Unexpected attribute(s) madeUpAttr</div></p>");
-        expect(mockGraph.setItems).not.toHaveBeenCalled();
+        expect(mockGraph.setData).not.toHaveBeenCalled();
         // Change the included tiddler
         wiki.addTiddler({title: "TiddlerOne", text: "World-wide Jelly"});
         // Refresh
         wiki.addTiddler({title: "TiddlerTwo", created: new Date(2014,2,15), modified: new Date(2014,2,31), text: "More stuff"});
         refreshWidgetNode(["TiddlerTwo"]);
         expect(wrapper.innerHTML).toBe("<p><div class='widget-error'>visjsgraph: Unexpected attribute(s) madeUpAttr</div></p>");
-        expect(mockGraph.setItems).not.toHaveBeenCalled();
+        expect(mockGraph.setData).not.toHaveBeenCalled();
       });
 
-      it("should render from a tiddlerList and default startDateField, setting start range to lowest", function() {
+      it("should render from AListTiddler", function() {
         // Construct the widget node
-        createAndRenderWidgetNode('<$visjsgraph tiddler="AListTiddler"/>');
+        createAndRenderWidgetNode('<$visjsgraph tiddler="AListTiddler" maxDepth="2"/>');
         // Test the rendering
-        expect(mockGraph.setItems).toHaveBeenCalledWith([ 
-          { id : 'TiddlerTwo', content : 'TiddlerTwo', start : new Date(2014,2,15), type : 'point' },
-          { id : 'TiddlerOne', content : 'TiddlerOne', start : new Date(2014,2,13), type : 'point' } ]);
-        expect(mockGraph.setWindow).toHaveBeenCalledWith(new Date(2014,2,13), undefined);
-        expect(mockGraph.setOptions).toHaveBeenCalledWith({showCustomTime: true});
+        expect(mockGraph.setData).toHaveBeenCalledWith(
+          { nodes : [ { id : 'AListTiddler', label : 'AListTiddler', shape : 'circle', color : undefined },
+            { id : 'TiddlerTwo', label : 'TiddlerTwo', shape : 'elipse', color : undefined },
+          { id : 'TiddlerOne', label : 'TiddlerOne', shape : 'elipse', color : undefined } ,
+          { id : '$:/DefaultTiddlers', label : '$:/DefaultTiddlers', shape : 'box', color : undefined } ],
+          edges : [ { from : 'AListTiddler', to : 'TiddlerTwo', label : 'list', style : 'arrow' },
+          { from : 'AListTiddler', to : 'TiddlerOne', label : 'list', style : 'arrow' },
+          { from : 'AListTiddler', to : '$:/DefaultTiddlers', label : 'list', style : 'arrow' },
+          ] });
         expect(mockGraph.on).toHaveBeenCalled();
-        expect(mockGraph.setGroups).not.toHaveBeenCalled();
-        expect(mockGraph.setCustomTime).not.toHaveBeenCalled();
+      });
+
+      it("should render from TiddlerThree", function() {
+        // Construct the widget node
+        createAndRenderWidgetNode('<$visjsgraph tiddler="TiddlerThree" maxDepth="2"/>');
+        // Test the rendering
+        expect(mockGraph.setData).toHaveBeenCalledWith(
+          { nodes : [ { id : 'TiddlerThree', label : 'TiddlerThree', shape : 'circle', color : undefined },
+            { id : 'TiddlerOne', label : 'TiddlerOne', shape : 'elipse', color : undefined } ],
+          edges : [ { from : 'TiddlerOne', to : 'TiddlerThree', label : 'tagging', style : 'arrow' } ] });
+        expect(mockGraph.on).toHaveBeenCalled();
+      });
+
+      it("should render from MissingTiddler", function() {
+        // Construct the widget node
+        createAndRenderWidgetNode('<$visjsgraph tiddler="MissingTiddler" maxDepth="2"/>');
+        // Test the rendering
+        expect(mockGraph.setData).toHaveBeenCalledWith(
+          { nodes : [ { id : 'MissingTiddler', label : 'MissingTiddler', shape : 'circle' },
+            { id : 'TiddlerOne', label : 'TiddlerOne', shape : 'elipse', color : undefined } ],
+          edges : [ { from : 'TiddlerOne', to : 'MissingTiddler', label : 'backlink', style : 'arrow' } ] });
+        expect(mockGraph.on).toHaveBeenCalled();
+      });
+
+      it("should render from TiddlerFour", function() {
+        // Construct the widget node
+        createAndRenderWidgetNode('<$visjsgraph tiddler="TiddlerFour" maxDepth="2"/>');
+        // Test the rendering
+        expect(mockGraph.setData).toHaveBeenCalledWith(
+          {nodes : [ { id : 'TiddlerFour', label : 'TiddlerFour', shape : 'circle', color : '#fe0' },
+           { id : 'TiddlerTwo', label : 'TiddlerTwo', shape : 'elipse', color : undefined },
+           { id : 'TiddlerOne', label : 'TiddlerOne', shape : 'elipse', color : undefined } ],
+           edges : [ { from : 'TiddlerTwo', to : 'TiddlerFour', label : 'backlink', style : 'arrow' },
+           { from : 'TiddlerOne', to : 'TiddlerFour', label : 'listed', style : 'arrow' } ] });
+        expect(mockGraph.on).toHaveBeenCalled();
+      });
+
+      it("should render from TiddlerTwo", function() {
+        // Construct the widget node
+        createAndRenderWidgetNode('<$visjsgraph tiddler="TiddlerTwo" maxDepth="2"/>');
+        // Test the rendering
+        expect(mockGraph.setData).toHaveBeenCalledWith(
+          { nodes : [ { id : 'TiddlerTwo', label : 'TiddlerTwo', shape : 'circle', color : undefined },
+            { id : 'TiddlerFour', label : 'TiddlerFour', shape : 'elipse', color : '#fe0' },
+          { id : 'TiddlerOne', label : 'TiddlerOne', shape : 'elipse', color : undefined },
+          { id : 'AListTiddler', label : 'AListTiddler', shape : 'elipse', color : undefined } ],
+          edges : [ { from : 'TiddlerTwo', to : 'TiddlerFour', label : 'link', style : 'arrow' },
+          { from : 'TiddlerTwo', to : 'TiddlerOne', label : 'tag', style : 'arrow' },
+          { from : 'TiddlerOne', to : 'TiddlerTwo', label : 'backlink', style : 'arrow' },
+          { from : 'AListTiddler', to : 'TiddlerTwo', label : 'listed', style : 'arrow' } ] });
+        expect(mockGraph.on).toHaveBeenCalled();
+      });
+
+
+      it("should render from TiddlerOne with maximum depth", function() {
+        // Construct the widget node
+        createAndRenderWidgetNode('<$visjsgraph tiddler="TiddlerOne"/>');
+        // Test the rendering
+        expect(mockGraph.setData).toHaveBeenCalledWith(
+          { nodes : [ { id : 'TiddlerOne', label : 'TiddlerOne', shape : 'circle', color : undefined },
+            { id : 'TiddlerTwo', label : 'TiddlerTwo', shape : 'elipse', color : undefined },
+          { id : 'TiddlerFour', label : 'TiddlerFour', shape : 'elipse', color : '#fe0' },
+          { id : 'AListTiddler', label : 'AListTiddler', shape : 'elipse', color : undefined },
+          { id : 'MissingTiddler', label : 'MissingTiddler', shape : 'triangle' },
+          { id : 'TiddlerThree', label : 'TiddlerThree', shape : 'elipse', color : undefined } ],
+          edges : [ { from : 'TiddlerOne', to : 'TiddlerTwo', label : 'backlink', style : 'arrow' },
+          { from : 'TiddlerTwo', to : 'TiddlerFour', label : 'link', style : 'arrow' },
+          { from : 'TiddlerTwo', to : 'TiddlerOne', label : 'tagging', style : 'arrow' },
+          { from : 'AListTiddler', to : 'TiddlerTwo', label : 'listed', style : 'arrow' },
+          { from : 'TiddlerOne', to : 'MissingTiddler', label : 'backlink', style : 'arrow' },
+          { from : 'TiddlerOne', to : 'TiddlerFour', label : 'list', style : 'arrow' },
+          { from : 'TiddlerOne', to : 'TiddlerThree', label : 'tagging', style : 'arrow' },
+          { from : 'AListTiddler', to : 'TiddlerOne', label : 'listed', style : 'arrow' } ] });
+        expect(mockGraph.on).toHaveBeenCalled();
       });
 
 
