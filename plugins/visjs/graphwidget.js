4a9b35946ef775d01fd4fb5fc7a3b699350975fa
Martin <martin@livetheseasons.com>
2014-03-16 10:28:40 +0000

    graph code refactoring

--- plugins/visjs/graphwidget.js
+++ plugins/visjs/graphwidget.js
@@ -19,12 +19,14 @@ module-type: widget
   var moment = require("$:/plugins/emkay/visjs/moment.js").moment;
   var vis = require("$:/plugins/emkay/visjs/vis.js").vis;
 
-  var backlinksFilter = $tw.wiki.compileFilter("[is[current]backlinks[]]");
-  var linksFilter = $tw.wiki.compileFilter("[is[current]links[]]");
-  var listFilter = $tw.wiki.compileFilter("[list[]]");
-  var listedFilter = $tw.wiki.compileFilter("[is[current]listed[]]");
-  var tagsFilter = $tw.wiki.compileFilter("[is[current]tags[]]");
-  var taggingFilter = $tw.wiki.compileFilter("[is[current]tagging[]]");
+  var compiledFilters = {
+    "backlink": $tw.wiki.compileFilter("[is[current]backlinks[]]"),
+    "link": $tw.wiki.compileFilter("[is[current]links[]]"),
+    "list": $tw.wiki.compileFilter("[list[]]"),
+    "listed": $tw.wiki.compileFilter("[is[current]listed[]]"),
+    "tag": $tw.wiki.compileFilter("[is[current]tags[]]"),
+    "tagging": $tw.wiki.compileFilter("[is[current]tagging[]]")
+  };
 
   var GraphWidget = function(parseTreeNode,options) {
     this.initialise(parseTreeNode,options);
@@ -51,7 +53,6 @@ module-type: widget
       this.refresh = function() {}; // disable refresh of this as it won't work with incorrrect attributes
     }
   };
-
 
   GraphWidget.prototype.parseWidgetAttributes = function(attributeDefns) {
     var errors = [];
@@ -86,25 +87,49 @@ module-type: widget
            maxDepth:       {   type: "integer", defaultValue: 3}
            // options:  { type: "flags", defaultValue: undefined}
             });
-
-    console.log(this.maxDepth);
     return attrParseWorked;
   };
 
-  function addNodes(compiledFilter, linkType, currTiddlerTitle, nodeAndEdgeSets, depth, maxDepth) {
-    var tiddlers = compiledFilter.call(null, null, currTiddlerTitle);
-    tiddlers.forEach(function(entry) {
-        nodeAndEdgeSets = buildNodeAndEdgeSets(nodeAndEdgeSets, currTiddlerTitle, linkType, entry, depth+1, maxDepth);
-    });
+  function addNode(nodeSet, tiddlerTitle, startingNode) {
+    nodeSet[tiddlerTitle] = {id: tiddlerTitle, label: tiddlerTitle, shape: "elipse"};
+    if ($tw.wiki.isSystemTiddler(tiddlerTitle)) {
+      nodeSet[tiddlerTitle].shape = "box";
+    }
+    var tiddler = $tw.wiki.getTiddler(tiddlerTitle);
+    if (tiddler === undefined) {
+      nodeSet[tiddlerTitle].shape = "triangle";
+    } else {
+      nodeSet[tiddlerTitle].color = tiddler.fields.color;
+    }
+    if (startingNode) {
+      nodeSet[tiddlerTitle].shape = "circle";
+    }
   }
-  // Types
-  // Tagging "[is[current]tagging[]]"
-  // Backlinks "[is[current]backlinks[]]"
-  // Forward links (through tag) "[is[current]tags[]]"
-  // 
-  // Do we want to do list and listed
-  //
-  // Also, what about doing an arbitraty graph? (Of heirarchical data)
+
+  function addEdge(edgeSet, throughLinkType, fromTiddlerTitle, toTiddlerTitle) {
+    if (fromTiddlerTitle !== null) {
+        var linkType = throughLinkType;
+        var tmp;
+        if (throughLinkType == "backlink") {
+          linkType = "link";
+          fromTiddlerTitle = [toTiddlerTitle, toTiddlerTitle = fromTiddlerTitle][0];
+        } else if (throughLinkType == "listed") {
+          linkType = "list";
+          fromTiddlerTitle = [toTiddlerTitle, toTiddlerTitle = fromTiddlerTitle][0];
+        } else if (throughLinkType == "tagging") {
+          linkType = "tag";
+          fromTiddlerTitle = [toTiddlerTitle, toTiddlerTitle = fromTiddlerTitle][0];
+        }
+        var lineStyle = "arrow";
+        // if (linkType == "list") {
+        //   lineStyle = "arrow-center";
+        // } else if (linkType = "tag") {
+        // }
+        edgeSet[fromTiddlerTitle+":"+toTiddlerTitle+":"+linkType] = {from: fromTiddlerTitle, to: toTiddlerTitle, label: linkType, style: lineStyle};
+    }
+  }
+
+    // http://visjs.org/examples/graph/04_shapes.html
   //
   // Encoding:
   // normal tiddlers
@@ -113,82 +138,38 @@ module-type: widget
   // system tiddlers
   // Shadow tiddlers
   //
+  function addNodesAndEdges(linkType, currTiddlerTitle, nodeAndEdgeSets, depth, maxDepth) {
+    var tiddlers = compiledFilters[linkType].call(null, null, currTiddlerTitle);
+    tiddlers.forEach(function(entry) {
+        nodeAndEdgeSets = buildNodeAndEdgeSets(nodeAndEdgeSets, currTiddlerTitle, linkType, entry, depth+1, maxDepth);
+    });
+  }
+
   function buildNodeAndEdgeSets(nodeAndEdgeSets, fromTiddlerTitle, linkType, toTiddlerTitle, depth, maxDepth) {
-    // if (nodeAndEdgeSets.nodeSet.hasOwnProperty(toTiddlerTitle) || (depth > maxDepth)) {
-    //   return nodeAndEdgeSets;
-    // }
     if (depth > maxDepth) {
       return nodeAndEdgeSets;
     }
     if (nodeAndEdgeSets.edgeSet.hasOwnProperty(fromTiddlerTitle+":"+toTiddlerTitle+":"+linkType)) {
       return nodeAndEdgeSets;
     }
-    var inverse = "";
-    if (linkType == "link") {
-      inverse = "backlink";
-    } else if (linkType == "backlink") {
-      inverse = "link";
-    } else if (linkType == "list") {
-      inverse = "listed";
-    } else if (linkType == "listed") {
-      inverse = "list";
-    } else if (linkType == "tag") {
-      inverse = "tagging";
-    } else if (linkType == "tagging") {
-      inverse = "tag";
-    }
-    // if (nodeAndEdgeSets.edgeSet.hasOwnProperty(toTiddlerTitle+":"+fromTiddlerTitle+":"+inverse)) {
-    //   return nodeAndEdgeSets;
-    // }
-    // http://visjs.org/examples/graph/04_shapes.html
-    if (fromTiddlerTitle !== null) {
-      if ( (linkType == "link") || (linkType == "list") || (linkType == "tag") ) {
-        nodeAndEdgeSets.edgeSet[fromTiddlerTitle+":"+toTiddlerTitle+":"+linkType] = {from: fromTiddlerTitle, to: toTiddlerTitle, label: linkType, style: "arrow"};
-      } else if ( (linkType == "backlink") || (linkType == "listed") || (linkType == "tagging") ) {
-        nodeAndEdgeSets.edgeSet[toTiddlerTitle+":"+fromTiddlerTitle+":"+inverse] = {from: toTiddlerTitle, to: fromTiddlerTitle, label: linkType, style: "arrow"};
-      }
-    }
-    if (nodeAndEdgeSets.nodeSet.hasOwnProperty(toTiddlerTitle)) {
-      return nodeAndEdgeSets;
-    }
+    addEdge(nodeAndEdgeSets.edgeSet, linkType, fromTiddlerTitle, toTiddlerTitle);
 
-    nodeAndEdgeSets.nodeSet[toTiddlerTitle] = {id: toTiddlerTitle, label: toTiddlerTitle, shape: "elipse"};
-    if ($tw.wiki.isSystemTiddler(toTiddlerTitle)) {
-      nodeAndEdgeSets.nodeSet[toTiddlerTitle].shape = "box";
-    }
-    var toTiddler = $tw.wiki.getTiddler(toTiddlerTitle);
-    if (toTiddler === undefined) {
-      nodeAndEdgeSets.nodeSet[toTiddlerTitle].shape = "triangle";
-    } else {
-      nodeAndEdgeSets.nodeSet[toTiddlerTitle].color = toTiddler.fields.color;
-    }
-    if (fromTiddlerTitle === null) {
-      console.log("start with "+toTiddlerTitle);
-      nodeAndEdgeSets.nodeSet[toTiddlerTitle].shape = "circle";
-      console.log(nodeAndEdgeSets.nodeSet[toTiddlerTitle]);
-    }
+    if (!nodeAndEdgeSets.nodeSet.hasOwnProperty(toTiddlerTitle)) {
+      addNode(nodeAndEdgeSets.nodeSet, toTiddlerTitle,fromTiddlerTitle === null);
 
-// > $tw.wiki.filterTiddlers("[is[current]links[]]","TiddlerTwo");
-// [ 'TiddlerFour' ]
-// > $tw.wiki.filterTiddlers("[links[]]","TiddlerTwo");
-// [ 'ShowGraph',
-//   'ShowTimelineDemo',
-//   'TiddlerTwo',
-//   'MissingTiddler',
-//   'TiddlerFour' ]
-    debugger;
-    // Forward links
-    addNodes(linksFilter, "link", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
-    // Forward links though list
-    addNodes(listFilter, "list", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
-    // Forward links though tag
-    addNodes(tagsFilter, "tag", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
-    // Backlinks
-    addNodes(backlinksFilter, "backlink", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
-    // Backlinks though listed
-    addNodes(listedFilter, "listed", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
-    // Backlinks though tags
-    addNodes(taggingFilter, "tagging", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
+      // Forward links
+      addNodesAndEdges("link", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
+      // Forward links though list
+      addNodesAndEdges("list", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
+      // Forward links though tag
+      addNodesAndEdges("tag", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
+      // Backlinks
+      addNodesAndEdges("backlink", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
+      // Backlinks though listed
+      addNodesAndEdges("listed", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
+      // Backlinks though tags
+      addNodesAndEdges("tagging", toTiddlerTitle, nodeAndEdgeSets, depth, maxDepth);
+    }
     return nodeAndEdgeSets;
   }
 
